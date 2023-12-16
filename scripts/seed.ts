987ccfc702ee9f699c8c5a3b239ddc4b734158f7
shayan heidary <hdrydeveloper@gmail.com>
2023-12-16 16:55:01 +0100

    DB - Prisma - Inserted Data

---
+++ scripts/seed.ts
@@ -0,0 +1,23 @@
+const { PrismaClient } = require("@prisma/client");
+
+const db = new PrismaClient();
+
+async function main() {
+  try {
+    await db.category.createMany({
+      data: [
+        { name: "Excel Reader", type: "Source" },
+        { name: "CSV Reader", type: "Source" },
+        { name: "Column Filter", type: "Manipulator" },
+        { name: "CSV Writer", type: "Predictor" },
+        { name: "CSV Writer", type: "Predictor" },
+      ],
+    });
+  } catch (err) {
+    console.log(err);
+  } finally {
+    db.$disconnect();
+  }
+}
+
+main();
